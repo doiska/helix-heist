2fd5bca385da35eaa22e969ae43d2282c8b64a58
doiska <pedro@twokei.com>
2025-11-27 20:03:17 -0300

    feat: helix json definitions

---
+++ types/helix-globals.lua
@@ -0,0 +1,17 @@
+---@class JSON
+JSON = JSON
+
+--- Parses a JSON string into a Lua table.
+---@param json string
+---@return table
+function JSON.parse(json)
+end
+
+--- Serializes a Lua table into a JSON string.
+---@param table table
+---@return string
+function JSON.stringify(table)
+end
+
+--@class HELIXTable
+--@param
