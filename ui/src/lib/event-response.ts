16dff185aeae083e740cfd4f5ea346afddfd256b
doiska <pedro@twokei.com>
2025-11-29 04:53:05 -0300

    feat: process callback response

---
+++ ui/src/lib/event-response.ts
@@ -0,0 +1,38 @@
+type CallbackResponse<Data = never> =
+  | {
+      status: "success";
+      data: Data;
+    }
+  | {
+      status: "error";
+      message: string;
+    };
+
+export function processCallbackResponse<Output = never>(
+  messageEvent: MessageEvent,
+) {
+  const response = messageEvent.data as { name: string; args?: string };
+
+  try {
+    if (!response.args) {
+      // imo it should always have a status
+      throw new Error("No status found in callback response");
+    }
+
+    return {
+      name: response.name,
+      data:
+        // Hack to get the first arg and make the fn work
+        typeof response.args === "string"
+          ? (JSON.parse(response.args)?.[0] as CallbackResponse<Output>)
+          : (response.args?.[0] as CallbackResponse<Output>),
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      name: response.name,
+      data: null,
+    };
+  }
+}
