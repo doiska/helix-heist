177fdc1678baecab7b2b7bb993fa029140d4a3c1
doiska <pedro@twokei.com>
2025-11-30 19:43:52 -0300

    feat: start door minigames

--- client/minigame.lua
+++ client/minigame.lua
@@ -1,107 +1,181 @@
 local activeMinigame = nil
-local Console = GetActorByTag('HConsole')
+local DOOR_INTERACT_RANGE = 50.0
 
-Console:RegisterCommand("startminigame", "Start a minigame", nil, {
-    HWorld,
-    function(_, _, args)
-        local minigameId = args[1]
-
-        if not minigameId then
-            print("/startminigame <id>")
-            print("vault_door, door_1, door_2")
-            return
-        end
-
-        TriggerCallback("StartMinigame", function(result)
-            if result.status ~= "success" then
-                print("Error: " .. (result.message or "Unknown"))
-                return
-            end
-
-            activeMinigame = result.data
-
-            if result.data.minigameType == "pattern" then
-                print("Use: /submitminigame vault_door 1 2 3 4")
-            elseif result.data.minigameType == "lockpick" then
-                print("Use: /submitminigame door_1 180")
-            end
-        end, minigameId)
+-- all 3 functions are not properly documented
+---@param doors { id: string, location: Vector }
+local function findClosestDoor(doors)
+    if not doors then
+        return nil
     end
-})
 
-Console:RegisterCommand("submitminigame", "Submit minigame attempt", nil, {
-    HWorld,
-    function(_, _, commandArgs)
-        local minigameId = commandArgs[1]
+    local playerLocation = GetPlayerLocation()
 
-        if not minigameId then
-            print("submitminigame <id> <attempt>")
-            return
-        end
+    if not playerLocation then
+        return nil
+    end
 
-        local args = table.unpack(commandArgs)
-        local attempt = nil
+    local closest = nil
 
-        if not activeMinigame then
-            print("No active minigame")
-            return
-        end
+    for _, door in ipairs(doors) do
+        local distance = HELIXMath.VectorDistance(playerLocation, door.location)
 
-        if activeMinigame and activeMinigame.minigameType == "pattern" then
-            if #args ~= 4 then
-                print("Pattern requires 4 digits")
-                return
-            end
-
-            attempt = {
-                tonumber(args[1]),
-                tonumber(args[2]),
-                tonumber(args[3]),
-                tonumber(args[4])
+        if distance <= DOOR_INTERACT_RANGE and (closest == nil or distance < closest.distance) then
+            closest = {
+                id = door.id,
+                distance = distance
             }
-        elseif activeMinigame and activeMinigame.minigameType == "lockpick" then
-            if #args ~= 1 then
-                print("Lockpick requires 1 number")
-                return
-            end
+        end
+    end
 
-            attempt = tonumber(args[1])
+    return closest
+end
+
+DebugCommand("startminigame", function(args)
+    local minigameId = args[1]
+
+    if not minigameId then
+        print("/startminigame <id>")
+        print("vault_door, door_1, door_2")
+        return
+    end
+
+    TriggerCallback("StartMinigame", function(result)
+        if result.status ~= "success" then
+            print("Error: " .. (result.message or "Unknown"))
+            return
         end
 
-        TriggerCallback("SubmitMinigameAttempt", function(result)
-            if result.status == "error" then
-                print("Error: " .. (result.message or "Unknown"))
-                return
-            end
+        activeMinigame = result.data
 
-            local data = result.data
+        if result.data.minigameType == "pattern" then
+            print("Use: /submitminigame vault_door 1 2 3 4")
+        elseif result.data.minigameType == "lockpick" then
+            print("Use: /submitminigame door_1 180")
+        end
+    end)
+end)
 
-            if data.result and data.solved then
-                activeMinigame = nil
-                return
-            end
-
-            if not data.solved and data.complete then
-                print("FAILED: " .. (data.message or "Max attempts"))
-                activeMinigame = nil
-                return
-            end
-
-            if activeMinigame.minigameType == "pattern" then
-                print(string.format(
-                    "Correct: %d Present: %d | Left: %d",
-                    data.result.correct,
-                    data.result.present,
-                    data.attemptsRemaining
-                ))
-            elseif activeMinigame.minigameType == "lockpick" then
-                print(string.format(
-                    "Diff: %d (%s) | Left: %d",
-                    data.result.difference,
-                    data.result.hint,
-                    data.attemptsRemaining
-                ))
-            end
-        end, minigameId, attempt)
+DebugCommand("heist:door", function()
+    if not CurrentHeist or (CurrentHeist.state ~= HeistStates.ENTRY and CurrentHeist.state ~= HeistStates.VAULT_LOCKED) then
+        HELIXTable.Dump(CurrentHeist)
+        print("Player is not in a Heist or not in entry/vault_locked states")
+        return
     end
-})
+
+    if not CurrentHeist.doors then
+        print("No access to current doors")
+    end
+
+    local door = findClosestDoor(CurrentHeist.doors)
+
+    if not door then
+        print("No bank door nearby")
+        return
+    end
+
+    TriggerCallback("StartMinigame", function(result)
+        if result.status ~= "success" then
+            print("Error: " .. (result.message or "Unknown"))
+            return
+        end
+
+        activeMinigame = result.data
+
+        if result.data.minigameType == "lockpick" then
+            print(string.format("Lockpick %s (Attempts left: %d) -> use /submitminigame %s <angle>", door.id,
+                result.data.attemptsRemaining, door.id))
+        else
+            print(string.format("Minigame %s ready (Attempts left: %d)", door.id, result.data.attemptsRemaining))
+        end
+    end, door.id)
+end)
+
+DebugCommand("submitminigame", function(commandArgs)
+    local minigameId = commandArgs[1]
+
+    if not minigameId then
+        print("submitminigame <id> <attempt>")
+        return
+    end
+
+    local args = {}
+
+    for i = 2, #commandArgs do
+        table.insert(args, commandArgs[i])
+    end
+
+    HELIXTable.Dump(commandArgs)
+    HELIXTable.Dump(args)
+
+    local attempt = nil
+
+    if not activeMinigame then
+        print("No active minigame")
+        return
+    end
+
+    if activeMinigame and activeMinigame.minigameType == "pattern" then
+        if #args ~= 4 then
+            print("Pattern requires 4 digits")
+            return
+        end
+
+        attempt = {
+            tonumber(args[1]),
+            tonumber(args[2]),
+            tonumber(args[3]),
+            tonumber(args[4])
+        }
+    elseif activeMinigame and activeMinigame.minigameType == "lockpick" then
+        if #args ~= 1 then
+            print("Lockpick requires 1 number")
+            return
+        end
+
+        attempt = tonumber(args[1])
+    end
+
+    TriggerCallback("SubmitMinigameAttempt", function(result)
+        if result.status == "error" then
+            print("Error: " .. (result.message or "Unknown"))
+            return
+        end
+
+        local data = result.data
+
+        if data.result and data.solved then
+            activeMinigame = nil
+            return
+        end
+
+        if not data.solved and data.complete then
+            print("FAILED: " .. (data.message or "Max attempts"))
+            activeMinigame = nil
+            return
+        end
+
+        if activeMinigame.minigameType == "pattern" then
+            print(string.format(
+                "Correct: %d Present: %d | Left: %d",
+                data.result.correct,
+                data.result.present,
+                data.attemptsRemaining
+            ))
+        elseif activeMinigame.minigameType == "lockpick" then
+            print(string.format(
+                "Diff: %d (%s) | Left: %d",
+                data.result.difference,
+                data.result.hint,
+                data.attemptsRemaining
+            ))
+        end
+    end, minigameId, attempt)
+end)
+
+RegisterClientEvent("HeistDoorOpened", function(data)
+    if not data then
+        return
+    end
+
+    print(string.format("[Heist] Door %s opened by %s", data.doorId or "unknown", data.openedBy or "unknown"))
+end)
