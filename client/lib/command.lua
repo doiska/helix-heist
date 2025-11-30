c311c92ccef92a96cc9efede7bdc8e84be85604d
doiska <pedro@twokei.com>
2025-11-30 14:59:52 -0300

    chore: move command to lib file

---
+++ client/lib/command.lua
@@ -0,0 +1,25 @@
+local Console = GetActorByTag('HConsole')
+local commands = {}
+
+function DebugCommand(name, cb)
+    print("Registering " .. name .. " command")
+    local commandInstance = Console:FindCommand(name)
+
+    if commandInstance then
+        Console:UnregisterCommand(commandInstance)
+    end
+
+    Console:RegisterCommand(name, name, nil, {
+        HWorld,
+        function(_, _, args)
+            cb(args)
+        end
+    })
+end
+
+function onShutdown()
+    for _, command in ipairs(commands) do
+        local commandInstance = Console:FindCommand(command)
+        Console:UnregisterCommand(commandInstance)
+    end
+end
