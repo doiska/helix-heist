7a4ec52c54a0939e79160c0aad17a79587fefb1e
doiska <pedro@twokei.com>
2025-11-30 20:31:09 -0300

    chore: disable door entity creation

--- server/entry.lua
+++ server/entry.lua
@@ -1,27 +1,27 @@
 print("Starting Heist script")
 
-local doors = {}
+-- local doors = {}
 
-for i, bank in pairs(Config.Banks) do
-    local bankName = Config.Banks[i].label
-    print('Loading ' .. bankName)
+-- for i, bank in pairs(Config.Banks) do
+--     local bankName = Config.Banks[i].label
+--     print('Loading ' .. bankName)
 
-    for _, door in ipairs(bank.security.doors) do
-        print('Spawning door ' .. door.entity .. ' (' .. (door.lockType or 'no lock') .. ')')
+--     for _, door in ipairs(bank.security.doors) do
+--         print('Spawning door ' .. door.entity .. ' (' .. (door.lockType or 'no lock') .. ')')
 
-        local SpawnTransform = Transform()
-        SpawnTransform.Translation = door.location
-        SpawnTransform.Rotation = door.rotation
+--         local SpawnTransform = Transform()
+--         SpawnTransform.Translation = door.location
+--         SpawnTransform.Rotation = door.rotation
 
-        doors[#doors + 1] = Door(DoorType.Classic, SpawnTransform, door.entity, {
-            bSupportsLocking = false,
-            bStartLocked = true
-        })
-    end
-end
+--         doors[#doors + 1] = Door(DoorType.Classic, SpawnTransform, door.entity, {
+--             bSupportsLocking = false,
+--             bStartLocked = true
+--         })
+--     end
+-- end
 
-function onShutdown()
-    for _, value in ipairs(doors) do
-        DeleteEntity(value)
-    end
-end
+-- function onShutdown()
+--     for _, value in ipairs(doors) do
+--         DeleteEntity(value)
+--     end
+-- end
