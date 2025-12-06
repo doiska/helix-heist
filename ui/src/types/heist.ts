fe73cc5166487b390ecfe288c66abd4611ac65fa
doiska <pedro@twokei.com>
2025-12-06 05:42:11 -0300

    refactor: use as const instead of enum

--- ui/src/types/heist.ts
+++ ui/src/types/heist.ts
@@ -6,20 +6,22 @@ export interface HeistLobby {
   canJoin: boolean;
 }
 
-enum HeistStates {
-  IDLE = "IDLE",
-  PREPARED = "PREPARED",
-  ENTRY = "ENTRY",
-  VAULT_LOCKED = "VAULT_LOCKED",
-  VAULT_OPEN = "VAULT_OPEN",
-  LOOTING = "LOOTING",
-  ESCAPE = "ESCAPE",
-  COMPLETE = "COMPLETE",
-  FAILED = "FAILED",
-}
+export const HeistStates = {
+  IDLE: "IDLE",
+  PREPARED: "PREPARED",
+  ENTRY: "ENTRY",
+  VAULT_LOCKED: "VAULT_LOCKED",
+  VAULT_OPEN: "VAULT_OPEN",
+  LOOTING: "LOOTING",
+  ESCAPE: "ESCAPE",
+  COMPLETE: "COMPLETE",
+  FAILED: "FAILED",
+} as const;
+
+export type HeistState = (typeof HeistStates)[keyof typeof HeistStates];
 
 export interface UserHeistState {
   inHeist: boolean;
-  state: HeistStates;
+  state: HeistState;
   heistId: string | null;
 }
